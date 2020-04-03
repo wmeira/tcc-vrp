d9f62b401db99a447cdce0668b2e8b45b78be805
William Hitoshi Tsunoda Meira <williammeira@gmail.com>
2020-04-03 15:05:49 -0300

    BingMaps key extracted from the repository

--- src/main/java/utfpr/tcc/vrp/prop/Path.java
+++ src/main/java/utfpr/tcc/vrp/prop/Path.java
@@ -12,12 +12,14 @@ public class Path {
 	private final static Logger logger = Logger.getLogger(Path.class.getName());
 	
 	private static Path path;
-	private File file = new File( "path.properties" );  
+	private File pathFile = new File( "path.properties" );
+	private File keyFile = new File( "key.properties" );
 	private String scenarioPath;
 	private String scenarioValidatorPath;
 	private String iconsPath;
 	private String modelsPath;
 	private String basePath;
+	private String bingKey;
 	
 	public static Path getInstance(){
 		if (path == null){
@@ -28,22 +30,29 @@ public class Path {
 	}
 	
 	private Path() {
-		Properties properties = new Properties();          
-        try {  
-            properties.load( new FileInputStream( file ) );  
+		Properties pathProperties = loadProperties(pathFile);
+		Properties keyProperties = loadProperties(keyFile);
+
+        scenarioPath = pathProperties.getProperty( "scenarioPath");      
+        scenarioValidatorPath = pathProperties.getProperty("scenarioValidatorPath");
+        iconsPath = pathProperties.getProperty("iconsPath");    
+        modelsPath = pathProperties.getProperty("modelsPath");
+        basePath = pathProperties.getProperty("basePath");
+        bingKey = keyProperties.getProperty("bingKey");
+	}
+	
+	private Properties loadProperties(File file) {
+		Properties properties = new Properties();
+		try {  
+			properties.load( new FileInputStream( file ) ); 
         } catch (FileNotFoundException ex) {  
-        	logger.severe("Não foi possível encontrar o arquivo path.properties.");
+        	logger.severe("Não foi possível encontrar o arquivo " + file.getName());
             ex.printStackTrace();  
         } catch (IOException ex) {  
-        	logger.severe("Erro de E/S ao ler o arquivo path.properties.\n" + ex.getMessage());
+        	logger.severe("Erro de E/S ao ler o arquivo " + file.getName() + ".\n" + ex.getMessage());
             ex.printStackTrace();  
-        }       
-        
-        scenarioPath = properties.getProperty( "scenarioPath");      
-        scenarioValidatorPath = properties.getProperty("scenarioValidatorPath");
-        iconsPath = properties.getProperty("iconsPath");    
-        modelsPath = properties.getProperty("modelsPath");
-        basePath = properties.getProperty("basePath");
+        }
+		return properties;
 	}
 
 	public String getScenarioPath() {		
@@ -64,5 +73,9 @@ public class Path {
 	
 	public String getBasePath() {
 		return basePath;
+	}
+	
+	public String getBingKey() {
+		return bingKey;
 	}
 }
